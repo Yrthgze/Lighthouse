794b4c596f065c40597a1cf605724560c2484386
Gontzal Aparicio <gontzal.aparicio@centum.com>
2024-08-02 15:02:15 +0200

    [TheLighthouse]: Basic lighthouse and light rotating

---
+++ scripts/lighthouse.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+
+@export var rotation_speed: float = 90.0 # Velocidad de rotación en grados por segundo
+
+func _process(delta: float) -> void:
+	var rotation_amount = 0.0
+
+	# Detectar entrada de las teclas
+	if Input.is_action_pressed("rotate_lighthouse_left"):
+		rotation_amount -= rotation_speed * delta
+	if Input.is_action_pressed("rotate_lighthouse_right"):
+		rotation_amount += rotation_speed * delta
+
+	# Aplicar la rotación al faro
+	$Lightbulb/SpotLight3D.rotate_y(rotation_amount * deg_to_rad(1))
