19963f3fbb9c79a31c5511693ba4847f5d94e8c0
Gontzal Aparicio <gontzal.aparicio@centum.com>
2024-08-08 15:52:36 +0200

    [TheLighthouse]: Hotfix

--- scripts/enemies/tentacle.gd
+++ scripts/enemies/tentacle.gd
@@ -1,11 +1,11 @@
 extends Character
 class_name Tentacle
 
-var is_in_attack_range:bool
+var in_attack_range:bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	is_in_attack_range = false
+	in_attack_range = false
 	$".".scale = Vector3(0.1,0.1,0.1)
 	$tentacle_grouped/AnimationPlayer.play("Idle")
 	anim_player = $tentacle_grouped/AnimationPlayer
@@ -15,10 +15,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	move_towards_target_pos(delta, target.position)
-	is_in_attack_range = is_in_attack_range()
-	if is_attacking and not is_in_attack_range:
+	in_attack_range = is_in_attack_range()
+	if is_attacking and not in_attack_range:
 		stop_attack()
-	if not is_attacking and is_in_attack_range:
+	if not is_attacking and in_attack_range:
 		start_attack()
 	
 func move_towards_target_pos(delta: float, target_position):
@@ -27,12 +27,12 @@ func move_towards_target_pos(delta: float, target_position):
 	look_at(target_position, Vector3.UP)
 
 func attack_animation():
-	print("Attak animation")
+	print("Attack animation")
 	#TODO anim_player.play("attack")
 	if particles:
 		particles.emitting = true
 	
 func attack_sound():
-	print("Attak sound")
+	print("Attack sound")
 	if audio_stream:
 		audio_stream.play()
