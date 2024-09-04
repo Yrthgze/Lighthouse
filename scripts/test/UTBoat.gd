91f673d75ffdc133747f10e36ffea8a36f385bc8
Gontzal Aparicio <gontzal.aparicio@centum.com>
2024-09-04 16:45:05 +0200

    [TheLighthouse]: UT for boat working, seems that bezier is fine.

--- scripts/test/UTBoat.gd
+++ scripts/test/UTBoat.gd
@@ -14,3 +14,7 @@ func test_get_turn_direction_point():
 	boat.look_at(Vector3(0, 0, 4))
 	var turn_dir_point1 = boat.get_turn_direction_point(Vector3(0, 0, 0), Vector3(6, 0, 6))
 	assert_vector3(Vector3(4, 0, 2), turn_dir_point1, "Turning point is not the expected")
+	
+	boat.look_at(Vector3(4, 0, 0))
+	var turn_dir_point2 = boat.get_turn_direction_point(Vector3(0, 0, 0), Vector3(6, 0, 6))
+	assert_vector3(Vector3(2, 0, 4), turn_dir_point2, "Turning point is not the expected")
