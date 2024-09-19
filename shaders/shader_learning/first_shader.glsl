7a628418aa3d7cbf43c112f70c562086e4fbf320
Gontzal Aparicio <gontzal.aparicio@centum.com>
2024-09-19 17:07:31 +0200

    [TheLighthouse]: Shaders learning, day 1: shader varying by time uniform in different channels

--- shaders/shader_learning/first_shader.glsl
+++ shaders/shader_learning/first_shader.glsl
@@ -2,10 +2,12 @@
 precision mediump float;
 #endif
 
-uniform float u_time;
+uniform vec2 u_resolution;  // Canvas size (width,height)
+uniform vec2 u_mouse;       // mouse position in screen pixels
+uniform float u_time;       // Time in seconds since load
 
 vec4 red_color(){
-    return vec4(1.0, 0.0, 0.0, 1.0) + vec4(0.0, 0.0, 1.0, 1.0);
+    return vec4(abs(sin(u_time)),cos(u_time),sin(u_time),1.0);
 }
 
 void main() {
